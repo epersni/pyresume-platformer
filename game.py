30b5b3e158807479c498182c1dafad99e9449d5b
nicklas <nicklas.m.persson@gmail.com>
2023-01-07 20:43:24 +0100

    Unlock experience feature

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from player import Player
 from level import Level
+from experience_screen import ExperienceScreen
 from background import Background
 from intro_screen import IntroScreen
 from enum import Enum
@@ -14,7 +15,7 @@ BACKGROUND_COLOR = (0, 0, 0)
 class GameState(Enum):
     INTRO = 1
     PLAY_LEVEL = 2
-    LEVEL_FINISHED = 3
+    SHOW_EXPERIENCES = 3
 
 
 class Game:
@@ -30,13 +31,12 @@ class Game:
         self.state = GameState.INTRO
         with open("levels.json", "r") as levels_config:
             self.config = json.load(levels_config)
-            print("loaded config")
         self.intro_screen = IntroScreen(self.config["intro"])
-        self._set_level(0)
-
-    def _set_level(self, stage_id):
-        self.level_config = self.config["levels"][stage_id]
-        self.level = Level(self.level_config, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.experience_screen = ExperienceScreen(self.config["levels"])
+        self.current_level_id = 0
+        self.level = Level(
+            self.config["levels"][self.current_level_id], SCREEN_WIDTH, SCREEN_HEIGHT
+        )
 
     def handle_inputs(self):
         pygame.event.pump()
@@ -56,24 +56,36 @@ class Game:
         elif self.state is GameState.INTRO:
             if key[pygame.K_RETURN]:
                 self.state = GameState.PLAY_LEVEL
+        elif self.state is GameState.SHOW_EXPERIENCES:
+            if key[pygame.K_RETURN]:
+                self.state = GameState.PLAY_LEVEL
+                next_level = self.experience_screen.selected_id
+                self.level = Level(
+                    self.config["levels"][next_level], SCREEN_WIDTH, SCREEN_HEIGHT
+                )
+            elif key[pygame.K_LEFT]:
+                self.experience_screen.show_prev()
+            elif key[pygame.K_RIGHT]:
+                self.experience_screen.show_next()
 
     def update(self):
-        self.player.update(self.level)
-        self.level.update(self.player)
-        if (
-            not self.level_is_moving and self.player.rect.top < SCREEN_HEIGHT / 2
-        ):  # TODO: magic number
-            self.level_is_moving = True
-            self.level.set_fall_speed(1)
+        if self.state is GameState.PLAY_LEVEL:
+            self.player.update(self.level)
+            self.level.update(self.player)
+            if (
+                not self.level_is_moving and self.player.rect.top < SCREEN_HEIGHT / 2
+            ):  # TODO: magic number
+                self.level_is_moving = True
+                self.level.set_fall_speed(1)
 
-        if self.player.is_dead():
-            self.is_running = False
+            if self.player.is_dead():
+                self.is_running = False
 
-        if self.level.completed:
-            # self.is_running = False
-            self.player = Player(300, 500)  # TODO from level design
-            self.level_is_moving = False
-            self._set_level(1)
+            if self.level.completed:
+                self.state = GameState.SHOW_EXPERIENCES
+                self.player = Player(300, 500)  # TODO from level design
+                self.level_is_moving = False
+                self.experience_screen.unlock_selected()
 
     def render(self):
         self.screen.fill(BACKGROUND_COLOR)
@@ -83,3 +95,5 @@ class Game:
             self.player.draw(self.screen)
         elif self.state is GameState.INTRO:
             self.intro_screen.draw(self.screen)
+        elif self.state is GameState.SHOW_EXPERIENCES:
+            self.experience_screen.draw(self.screen)
